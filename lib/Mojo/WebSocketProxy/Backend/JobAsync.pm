ddd9f0ed7cdd86da9bbf651b79935a45b782abf3
Nael Alolwani <nael@binary.com>
2019-07-04 13:06:51 +0000

    Fix Everything

--- lib/Mojo/WebSocketProxy/Backend/JobAsync.pm
+++ lib/Mojo/WebSocketProxy/Backend/JobAsync.pm
@@ -9,6 +9,7 @@ no indirect;
 
 use IO::Async::Loop::Mojo;
 use Job::Async;
+use MojoX::JSON::RPC::Client;
 use JSON::MaybeUTF8 qw(encode_json_utf8 decode_json_utf8);
 
 use Log::Any qw($log);
@@ -57,7 +58,7 @@ sub new {
     my ($class, %args) = @_;
     # Avoid holding these - we only want the Job::Async::Client instance, and everything else
     # should be attached to the loop (which sticks around longer than we expect to).
-    my $loop   = delete $args{loop};
+    my $loop = delete $args{loop};
     my $jobman = delete $args{jobman};
 
     my $self = bless \%args, $class;
@@ -66,14 +67,16 @@ sub new {
     # other systems, so any combination of Job::Async::Client, Job::Async and/or IO::Async::Loop
     # instance can be provided here.
     $self->{client} // do {
-        unless ($jobman) {
+        unless($jobman) {
             # We don't hold a ref to this, since that might introduce unfortunate cycles
             $loop //= do {
                 require IO::Async::Loop::Mojo;
                 IO::Async::Loop::Mojo->new;
             };
             $self->{loop} = $loop;
-            $loop->add($jobman = Job::Async->new);
+            $loop->add(
+                $jobman = Job::Async->new
+            );
         }
 
         $self->{client} = $jobman->client(redis => $self->{redis});
@@ -103,9 +106,8 @@ Implements the L<Mojo::WebSocketProxy::Backend/call_rpc> interface.
 
 sub call_rpc {
     my ($self, $c, $req_storage) = @_;
-    my $method = $req_storage->{method};
+    my $method   = $req_storage->{method};
     my $msg_type = $req_storage->{msg_type} ||= $req_storage->{method};
-    # Prepare client before submitting a call. 
     $self->{client}->start->get;
 
     $req_storage->{call_params} ||= {};
@@ -122,35 +124,38 @@ sub call_rpc {
 
     $self->client->submit(
         name   => $req_storage->{name},
-        params => encode_json_utf8($params) 
-        )->on_ready(
-        sub {
-            my ($f) = @_;
-            $log->debugf('->submit completion: ', $f->state);
+        params => encode_json_utf8($params)
+    )->on_ready(sub {
+        my ($f) = @_;
+        $log->debugf('->submit completion: ', $f->state);
 
-            $_->($c, $req_storage) for @$before_get_rpc_response_hook;
+        $_->($c, $req_storage) for @$before_get_rpc_response_hook;
 
-            # unconditionally stop any further processing if client is already disconnected
-            return Future->done unless $c and $c->tx;
+        # unconditionally stop any further processing if client is already disconnected
+        return Future->done unless $c and $c->tx;
 
-            my $api_response;
-            if ($f->is_done) {
-                use MojoX::JSON::RPC::Client;
-                my $result = MojoX::JSON::RPC::Client::ReturnObject->new(
-                    rpc_response => decode_json_utf8($f->get)
-                );
+        my $api_response;
+        if($f->is_done) {
+            my $result = MojoX::JSON::RPC::Client::ReturnObject->new(
+                rpc_response => decode_json_utf8($f->get)
+            );
 
-                $_->($c, $req_storage, $result) for @$after_got_rpc_response_hook;
-                $api_response = $rpc_response_cb->($result->result);
-            } else {
-                my ($failure) = $f->failure;
-                $log->warnf("method %s failed: %s", $method, $failure);
+            $_->($c, $req_storage, $result) for @$after_got_rpc_response_hook;
 
-                $api_response = $c->wsp_error($msg_type, 'WrongResponse', 'Sorry, an error occurred while processing your request.');
-            }
-            return unless $api_response;
-            $c->send({json => $api_response}, $req_storage);
-        })->retain;
+            $api_response = $rpc_response_cb->($result->result);
+        }
+        else {
+            my ($failure) = $f->failure;
+            $log->warnf("method %s failed: %s", $method, $failure);
+
+            $api_response = $c->wsp_error(
+                $msg_type, 'WrongResponse', 'Sorry, an error occurred while processing your request.'
+            );
+        }
+        return unless $api_response;
+
+        $c->send({json => $api_response}, $req_storage);
+    })->retain;
     return;
 }
 
--- t/25-job-async-backend.t
+++ t/25-job-async-backend.t
@@ -18,12 +18,12 @@ package t::SampleClient {
     use parent qw(Job::Async::Client);
 
     sub loop { shift->{loop} //= $loop }
-    sub start { my ($self) = @_; }
+    sub start { my ($self) = @_; return $self->loop->new_future->done; }
 
     sub submit {
         my ($self, %args) = @_;
         push @::PENDING_JOBS, my $job = Job::Async::Job->new(
-            data   => $args{data},
+            data   => {name => $args{name}, params => $args{params}},
             id     => ++$LAST_ID,
             future => $self->loop->new_future,
         );
@@ -47,7 +47,7 @@ package t::SampleWorker {
             }
             return Future->done;
         } while => sub { 0 + @::PENDING_JOBS }
-    }
+        }
 
     sub process {
         my ($self, $job) = @_;
@@ -98,13 +98,13 @@ test_wsp {
             sub {
                 my ($job) = @_;
                 is($job->id, 1000, 'have correct ID');
-                is($job->data('method'), 'faraway', 'method was correct');
+                is($job->data('name'), 'faraway', 'method was correct');
                 $job->done(encode_json_utf8({result => 'everything worked'}));
             },
             sub {
                 my ($job) = @_;
                 is($job->id, 1001, 'have correct ID');
-                is($job->data('method'), 'faraway', 'method was correct');
+                is($job->data('name'), 'faraway', 'method was correct');
                 $job->fail('everything broke');
             }
         );
