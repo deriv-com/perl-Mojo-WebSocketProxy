f7c0f684425411e838a83fe5c9dd107fc19abe9d
Mat Sepahi <mat@firstsource.io>
2019-07-16 06:15:10 +0000

    fix unless ()

--- lib/Mojo/WebSocketProxy/Backend/JobAsync.pm
+++ lib/Mojo/WebSocketProxy/Backend/JobAsync.pm
@@ -66,7 +66,7 @@ sub new {
     # We'd like to provide some flexibility for people trying to integrate this into
     # other systems, so any combination of Job::Async::Client, Job::Async and/or IO::Async::Loop
     # instance can be provided here.
-    unless $self->{client} {
+    unless ($self->{client}) {
         unless($jobman) {
             # We don't hold a ref to this, since that might introduce unfortunate cycles
             $loop //= do {
