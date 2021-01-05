88dffd6635f3c87641ec20456dc8f8e375919c69
Kirill Sysoev <55120154+kirill-binary@users.noreply.github.com>
2021-01-05 16:27:06 +0800

    Update lib/Mojo/WebSocketProxy/Backend/ConsumerGroups.pm
    Co-authored-by: nima <69203365+nima-fs@users.noreply.github.com>

--- lib/Mojo/WebSocketProxy/Backend/ConsumerGroups.pm
+++ lib/Mojo/WebSocketProxy/Backend/ConsumerGroups.pm
@@ -210,7 +210,7 @@ sub call_rpc {
     my $after_got_rpc_response_hooks  = delete($req_storage->{after_got_rpc_response}) || [];
     my $before_call_hooks             = delete($req_storage->{before_call}) || [];
     my $rpc_failure_cb                = delete($req_storage->{rpc_failure_cb});
-    # stream category$self->queue_separation_enabled which message should be assigned to
+    # stream category which message should be assigned to
     $req_storage->{msg_group} = $self->queue_separation_enabled && $req_storage->{msg_group} ? $req_storage->{msg_group} : DEFAULT_CATEGORY_NAME;
 
     foreach my $hook ($before_call_hooks->@*) { $hook->($c, $req_storage) }
