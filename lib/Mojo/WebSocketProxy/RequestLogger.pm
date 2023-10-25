4cf7521adeda3532260d86a78e4e23f5399ae650
aisha-deriv <aisha@deriv.com>
2023-10-25 14:40:55 +0800

    request handler added

--- lib/Mojo/WebSocketProxy/RequestLogger.pm
+++ lib/Mojo/WebSocketProxy/RequestLogger.pm
@@ -8,63 +8,86 @@ use Log::Any qw($log);
 use UUID::Tiny;
 
 field $context;
+
+our $default_handler = sub {
+    my ($level, $message, $context, @params) = @_;
+
+    if(scalar @params) {
+        return $log->$level($message, @params);
+    }
+
+    return $log->$level($message, $context);
+};
+
+sub set_handler {
+    my ($self, $custom_handler) = @_;
+    $default_handler = $custom_handler;
+} 
+
 BUILD{
     $context->{correlation_id} =  UUID::Tiny::create_UUID_as_string(UUID::Tiny::UUID_V4);
 }
 
-# Method to log messages with various log levels
-method log_message($level, $message, @params) {
-    if($log->adapter->can('set_context')) {
-        $log->adapter->set_context($context);
-    }
-    $log->$level($message, @params);
-    if($log->adapter->can('clear_context')) {
-        $log->adapter->clear_context;
-    }
-}
-
 method infof($message, @params) {
-    $self->log_message('infof', $message, @params);
+    $default_handler->('infof', $message, $context, @params);
 }
 
 method tracef($message, @params) {
-    $self->log_message('tracef', $message, @params);
+    $default_handler->('tracef', $message, $context, @params);
 }
 
 method errorf($message, @params) {
-    $self->log_message('errorf', $message, @params);
+    $default_handler->('errorf', $message, $context, @params);
 }
 
 method warnf($message, @params) {
-    $self->log_message('warningf', $message, @params);
+    $default_handler->('warningf', $message, $context, @params);
 }
 
 method debugf($message, @params) {
-    $self->log_message('debugf', $message, @params);
+    $default_handler->('debugf', $message, $context, @params);
+}
+
+method critf($message, @params) {
+    $default_handler->('critf', $message, $context, @params);
 }
 
 method info($message) {
-    $self->log_message('info', $message);
+    use Data::Dumper;
+    print 'here to print message';
+    $default_handler->('info', $message, $context);
 }
 
 method trace($message) {
-    $self->log_message('trace', $message);
+    $default_handler->('trace', $message , $context);
 }
 
 method error($message) {
-    $self->log_message('error', $message);
+    $default_handler->('error', $message, $context);
 }
 
 method warn($message) {
-    $self->log_message('warning', $message);
+    $default_handler->('warning', $message, $context);
 }
 
 method debug($message) {
-    $self->log_message('debug', $message);
+    $default_handler->('debug', $message, $context);
+}
+
+method crit($message) {
+    $default_handler->('crit', $message, $context);
 }
 
 method get_context(){
     return $context;
+}
+
+method add_context_key($key, $value) {
+    $context->{$key} = $value;
+}
+
+method remove_context_key($key){
+    delete $context->{$key};
 }
 
 1;
