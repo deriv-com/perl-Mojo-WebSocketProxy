3d66d9cc60e77b8e081343bb69012833762ed16c
Denis Safiullin <denis.safiullin@regentmarkets.com>
2023-12-06 03:50:21 +0000

    replace debug with debugf

--- lib/Mojo/WebSocketProxy/Dispatcher.pm
+++ lib/Mojo/WebSocketProxy/Dispatcher.pm
@@ -50,8 +50,9 @@ sub ok {
 sub open_connection {
     my ($c) = @_;
 
-    my $log = $c->app->log;
-    $log->debug("accepting a websocket connection from " . ($c->tx->remote_address // 'undefined remote address'));
+    my $log            = $c->app->log;
+    my $remote_address = $c->$tx->remote_address // 'undefined remote address';
+    $log->debugf('accepting a websocket connection from %s', $remote_address);
     # Enable permessage-deflate
     $c->tx->with_compression;
 
